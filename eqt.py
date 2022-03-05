cec0cbcb30cb973bb3c6508ab7fda64c668354fa
ssaitou126 <ssaitou126@gmail.com>
2022-03-05 10:11:06 +0900

    timezone確認 sine削除

--- eqt.py
+++ eqt.py
@@ -2,8 +2,19 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 st.subheader('eqation')
+
+ntoday = datetime.datetime.now()
+nstr = ntoday.strftime('%Y-%m-%d  %H:%M:%S')
+st.write(f'現在N時刻は{nstr}です')
+atoday = datetime.datetime.now(datetime.timezone.utc)
+astr = atoday.strftime('%Y-%m-%d  %H:%M:%S')
+st.write(f'現在A時刻は{astr}です')
+jtoday = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
+jstr = jtoday.strftime('%Y-%m-%d  %H:%M:%S')
+st.write(f'現在J時刻は{jstr}です')
 
 val = st.slider('valiable',1,5,1)
 
